a1d88a48bf25ce958f7a66bb57ef9da714b980fc
personal <me@grantbrooks.io>
2024-10-04 19:40:53 -0700

    Use `AppSubsystem` package dependency for core models & services

--- Application/Localization/Localization.swift
+++ Application/Localization/Localization.swift
@@ -8,8 +8,8 @@
 /* Native */
 import Foundation
 
-/* 3rd-party */
-import CoreArchitecture
+/* Proprietary */
+import AppSubsystem
 
 public enum Localization {
     // MARK: - Types
@@ -41,6 +41,25 @@ public enum Localization {
         return dictionary
     }
 
+    // MARK: - Initialize
+
+    public static func initialize() {
+        @Dependency(\.coreKit.utils) var coreUtilities: CoreKit.Utilities
+
+        let unsupportedLanguageCodes = ["ba", "ceb", "jv", "la", "mr", "ms", "udm"]
+        let supportedLanguages = localizedStrings["language_codes"]?.filter { !unsupportedLanguageCodes.contains($0.key) } ?? [:]
+        RuntimeStorage.store(supportedLanguages, as: .languageCodeDictionary)
+
+        if RuntimeStorage.languageCodeDictionary?[RuntimeStorage.languageCode] == nil || supportedLanguages.isEmpty {
+            Logger.log(.init(
+                "Unsupported language code; reverting to English.",
+                metadata: [self, #file, #function, #line]
+            ))
+
+            coreUtilities.setLanguageCode("en")
+        }
+    }
+
     // MARK: - String for Key
 
     public static func string(
@@ -60,5 +79,25 @@ public enum Localization {
 
     public static func clearCache() {
         cachedLocalizedStrings = nil
+    }
+}
+
+public extension Localization {
+    struct LocalizedStringsDelegate: AppSubsystem.Delegates.LocalizedStringsDelegate {
+        public var cancel: String { Localized(.cancel).wrappedValue }
+        public var done: String { Localized(.done).wrappedValue }
+        public var dismiss: String { Localized(.dismiss).wrappedValue }
+        public var internetConnectionOffline: String { Localized(.internetConnectionOffline).wrappedValue }
+        public var noEmail: String { Localized(.noEmail).wrappedValue }
+        public var noInternetMessage: String { Localized(.noInternetMessage).wrappedValue }
+        public var reportBug: String { Localized(.reportBug).wrappedValue }
+        public var reportSent: String { Localized(.reportSent).wrappedValue }
+        public var sendFeedback: String { Localized(.sendFeedback).wrappedValue }
+        public var settings: String { Localized(.settings).wrappedValue }
+        public var somethingWentWrong: String { Localized(.somethingWentWrong).wrappedValue }
+        public var tapToReport: String { Localized(.tapToReport).wrappedValue }
+        public var timedOut: String { Localized(.timedOut).wrappedValue }
+        public var tryAgain: String { Localized(.tryAgain).wrappedValue }
+        public var yesterday: String { Localized(.yesterday).wrappedValue }
     }
 }
--- Application/Modules/Content/Sample/Views/EmptyPageView/EmptyPageReducer.swift
+++ Application/Modules/Content/Sample/Views/EmptyPageView/EmptyPageReducer.swift
@@ -9,8 +9,8 @@
 /* Native */
 import Foundation
 
-/* 3rd-party */
-import CoreArchitecture
+/* Proprietary */
+import AppSubsystem
 
 public struct EmptyPageReducer: Reducer {
     // MARK: - Actions
