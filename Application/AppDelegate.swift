623cd75402d17f5a0c8e58c8f01a95933c6ba6e0
personal <me@grantbrooks.io>
2024-10-28 14:32:01 -0700

    Use `AppSubsystem` package dependency for core models & services

--- Application/AppDelegate.swift
+++ Application/AppDelegate.swift
@@ -10,133 +10,16 @@
 import Foundation
 import UIKit
 
-/* 3rd-party */
-import AlertKit
-import CoreArchitecture
-import Translator
-
 @main
 public final class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: - UIApplication
 
     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        preInitialize()
-
-        /* Encapsulate further work here into setup functions. */
-
+        Application.initialize()
         return true
     }
 
     public func applicationDidBecomeActive(_ application: UIApplication) {}
-
-    // MARK: - Initialization + Setup
-
-    private func preInitialize() {
-        /* MARK: Dependencies */
-
-        @Dependency(\.alertKitConfig) var alertKitConfig: AlertKit.Config
-        @Dependency(\.breadcrumbs) var breadcrumbs: Breadcrumbs
-        @Dependency(\.build) var build: Build
-        @Dependency(\.coreKit) var core: CoreKit
-        @Dependency(\.translatorConfig) var translatorConfig: Translator.Config
-
-        /* MARK: Defaults Keys & Logging Setup */
-
-        RuntimeStorage.store(BuildConfig.languageCode, as: .languageCode)
-
-        Logger.setDomainsExcludedFromSessionRecord(BuildConfig.loggerDomainsExcludedFromSessionRecord)
-        Logger.subscribe(to: BuildConfig.loggerDomainSubscriptions)
-
-        @Persistent(.breadcrumbsCaptureEnabled) var breadcrumbsCaptureEnabled: Bool?
-        @Persistent(.breadcrumbsCapturesAllViews) var breadcrumbsCapturesAllViews: Bool?
-        if build.stage == .generalRelease {
-            breadcrumbsCaptureEnabled = false
-            breadcrumbsCapturesAllViews = nil
-        } else if let breadcrumbsCaptureEnabled,
-                  let breadcrumbsCapturesAllViews,
-                  breadcrumbsCaptureEnabled {
-            breadcrumbs.startCapture(uniqueViewsOnly: !breadcrumbsCapturesAllViews)
-        }
-
-        @Persistent(.hidesBuildInfoOverlay) var hidesBuildInfoOverlay: Bool?
-        if hidesBuildInfoOverlay == nil {
-            hidesBuildInfoOverlay = false
-        }
-
-        /* MARK: Developer Mode Setup */
-
-        DevModeService.addStandardActions()
-        DevModeService.addCustomActions()
-
-        /* MARK: Theme Setup */
-
-        @Persistent(.pendingThemeID) var pendingThemeID: String?
-        @Persistent(.currentThemeID) var currentThemeID: String?
-
-        if let themeID = pendingThemeID,
-           let correspondingCase = AppTheme.allCases.first(where: { $0.theme.encodedHash == themeID }) {
-            ThemeService.setTheme(correspondingCase.theme, checkStyle: false)
-            pendingThemeID = nil
-        } else if let currentThemeID,
-                  let correspondingCase = AppTheme.allCases.first(where: { $0.theme.encodedHash == currentThemeID }) {
-            ThemeService.setTheme(correspondingCase.theme, checkStyle: false)
-        } else {
-            ThemeService.setTheme(AppTheme.default.theme, checkStyle: false)
-        }
-
-        /* MARK: AlertKit & Translator Setup */
-
-        alertKitConfig.overrideTargetLanguageCode(RuntimeStorage.languageCode)
-        alertKitConfig.overrideTranslationHUDConfig(.init(appearsAfter: .milliseconds(500), isModal: true))
-
-        alertKitConfig.registerLoggerDelegate(Logger.AlertKitLogger())
-        alertKitConfig.registerPresentationDelegate(core)
-
-        ReportDelegate.registerWithDependencies()
-        TranslationDelegate.registerWithDependencies()
-
-        LocalTranslationArchiverDelegate.registerWithDependencies()
-        translatorConfig.registerLoggerDelegate(Logger.TranslationLogger())
-
-        /* MARK: Navigation Setup */
-
-        let navigationCoordinator: NavigationCoordinator<RootNavigationService> = .init(
-            .init(modal: .splash),
-            navigating: RootNavigationService()
-        )
-
-        NavigationCoordinatorResolver.shared.store(navigationCoordinator)
-
-        /* MARK: Localization Setup */
-
-        let localizedStrings = Localization.localizedStrings
-        guard !localizedStrings.isEmpty else {
-            Logger.log(.init("Missing localized strings.", metadata: [self, #file, #function, #line]))
-            return
-        }
-
-        let unsupportedLanguageCodes = ["ba", "ceb", "jv", "la", "mr", "ms", "udm"]
-        let supportedLanguages = localizedStrings["language_codes"]?.filter { !unsupportedLanguageCodes.contains($0.key) }
-        guard let supportedLanguages else {
-            Logger.log(.init("No supported languages.", metadata: [self, #file, #function, #line]))
-            return
-        }
-
-        RuntimeStorage.store(supportedLanguages, as: .languageCodeDictionary)
-        guard let languageCodeDictionary = RuntimeStorage.languageCodeDictionary else { return }
-
-        guard languageCodeDictionary[RuntimeStorage.languageCode] != nil else {
-            Logger.log(
-                .init(
-                    "Unsupported language code; reverting to English.",
-                    metadata: [self, #file, #function, #line]
-                )
-            )
-
-            core.utils.setLanguageCode("en")
-            return
-        }
-    }
 
     // MARK: - UISceneSession
 
--- Application/Navigation/RootNavigationService.swift
+++ Application/Navigation/RootNavigationService.swift
@@ -9,8 +9,8 @@
 /* Native */
 import Foundation
 
-/* 3rd-party */
-import CoreArchitecture
+/* Proprietary */
+import AppSubsystem
 
 public struct RootNavigationService: Navigating {
     // MARK: - Type Aliases
