97c5f702a2e16c1f9bac9f5fdb2986b0e2ec5390
personal <me@grantbrooks.io>
2024-11-21 11:53:26 -0800

    Use `AppSubsystem` package dependency for core models & services

---
+++ Application/Bundle/UserDefaultsKeys.swift
@@ -0,0 +1,17 @@
+//
+//  UserDefaultsKeys.swift
+//  Template
+//
+//  Created by Grant Brooks Goodman on DD/MM/20YY.
+//  Copyright © 2013-20YY NEOTechnica Corporation. All rights reserved.
+//
+
+/* Native */
+import Foundation
+
+/* Proprietary */
+import AppSubsystem
+
+public extension UserDefaultsKey {
+    /* Add values here for each new defaults key. */
+}
